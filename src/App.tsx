8b86160b19a9904e444b6e6d92ac7a82b354c0fd
teembama <144135011+teembama@users.noreply.github.com>
2024-06-04 15:12:45 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HelloWorld from './components/HelloWorld';
+const App: React.FC = () => {
+  return (
+    <div className= "bg-indigo-100 h- 48 flex justify-center items-center">
+      <HelloWorld />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const HelloWorld: React.FC = () => {
+    return (
+        <div className=
+            "h-screen flex justifycenter items-center">
+            <p>I am just saying Hello World
+                from React with TypeScript</p>
+        </div>
+    )
+}
+export default HelloWorld;
